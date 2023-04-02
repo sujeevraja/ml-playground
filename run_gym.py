34c08a2ce031fef9c0eeb4075314649f7e350f52
Sujeevraja Sanjeevi <sujeev@protonmail.com>
2023-04-02 08:59:39 -0700

    add script with sample gym run

---
+++ run_gym.py
@@ -0,0 +1,22 @@
+"""Run this script to see some ways to train RL models with gymnasium"""
+
+import gymnasium as gym
+
+
+def try_gym():
+    env = gym.make("LunarLander-v2", render_mode="human")
+    observation, info = env.reset()
+
+    for _ in range(5000):
+        # agent policy that uses the observation and info
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+
+
+if __name__ == '__main__':
+    try_gym()
